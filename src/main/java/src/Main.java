54223d06669de7d6d07d6e4992bc78c6ad8894e1
unknown <samsiggs00@gmail.com>
2019-08-14 20:01:12 +1200

    Set up menu and added placeholers

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -2,13 +2,7 @@ package src;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -23,13 +17,16 @@ public class Main extends Application {
         BorderPane layout = new BorderPane();
         Scene scene = new Scene(layout, 500, 800);
 
+
+
+
         // Layouts for borderpane - feel free to change
         VBox topPane = new VBox();
+        topPane.getChildren().addAll(new MenuMain().getMenu());
+
         VBox bottomPane = new VBox();
         VBox leftPane = new VBox();
         VBox rightPane = new VBox();
-
-
 
         layout.setTop(topPane);
         layout.setBottom(bottomPane);
