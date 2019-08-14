5e2395954e7e2e895bfb47d77eb05119c5f4f75b
unknown <samsiggs00@gmail.com>
2019-08-14 19:28:20 +1200

    Added Layouts for borderpane, we may choose to not use some in that case we can remove them later

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -3,7 +3,13 @@ package src;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,14 +20,23 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Untitled - Ultimate Text Editor 9000");
-
-
         BorderPane layout = new BorderPane();
         Scene scene = new Scene(layout, 500, 800);
+
+        // Layouts for borderpane - feel free to change
+        VBox topPane = new VBox();
+        VBox bottomPane = new VBox();
+        VBox leftPane = new VBox();
+        VBox rightPane = new VBox();
+
+
+
+        layout.setTop(topPane);
+        layout.setBottom(bottomPane);
+        layout.setLeft(leftPane);
+        layout.setRight(rightPane);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-
 
 }
