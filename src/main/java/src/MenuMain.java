4c992228867850570f592b280103a307592b89c6
unknown <samsiggs00@gmail.com>
2019-08-20 07:22:55 +1200

    Added about box with the specifications in the assignment

--- src/main/java/src/MenuMain.java
+++ src/main/java/src/MenuMain.java
@@ -1,9 +1,20 @@
 package src;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import org.fxmisc.richtext.CodeArea;
 
 public class MenuMain
@@ -14,6 +25,8 @@ public class MenuMain
     private MenuItem newFile;
     private MenuItem openFile;
     private MenuItem saveFile;
+    private Menu helpMenu;
+    private MenuItem about;
 
     public MenuMain()
     {
@@ -22,9 +35,12 @@ public class MenuMain
         this.newFile = new MenuItem("New");
         this.openFile = new MenuItem("Open");
         this.saveFile = new MenuItem("Save");
+        this.helpMenu = new Menu("Help");
+        this.about = new MenuItem("About");
 
         fileMenu.getItems().addAll(newFile, openFile, saveFile);
-        menu.getMenus().addAll(fileMenu);
+        helpMenu.getItems().addAll(about);
+        menu.getMenus().addAll(fileMenu, helpMenu);
 
         // Setting actions
         newFile.setOnAction(event ->
@@ -55,6 +71,32 @@ public class MenuMain
         saveFile.setOnAction(event ->
         {
             new FileIO().Save(getEditor().getText(), getEditor().getScene().getWindow());
+        });
+
+        about.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                try {
+                    VBox root = new VBox();
+                    root.setAlignment(Pos.TOP_CENTER);
+                    Label title = new Label("Ultimate Text Editor 9000");
+                    title.setFont(new Font(20.0));
+                    title.setPadding(new Insets(10,0,5,0));
+                    Label createdBy = new Label("Creators:\nJurgen\nSam Siggs");
+                    createdBy.setFont(new Font(12.0));
+                    Label description = new Label("breif message");
+                    description.setPadding(new Insets(10,0,0,0));
+
+                    root.getChildren().addAll(title, createdBy, description);
+
+
+                    Stage stage = new Stage();
+                    stage.setTitle("Ultimate Text Editor 9000");
+                    stage.setScene(new Scene(root, 325, 150));
+                    stage.show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         });
     }
 
