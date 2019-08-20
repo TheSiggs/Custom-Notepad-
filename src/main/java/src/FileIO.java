b5408268c564bb597dbd32c61b48cc5483896e47
Jurgen <rjrooyen@mail.com>
2019-08-20 18:35:02 +1200

    Implemented tika detection and parsing
    Using tika to detect file type and parse, needs testing but should theoretically support .docx, .xml etc files.
    
    OpenText removed as functionality covered by tika

--- src/main/java/src/FileIO.java
+++ src/main/java/src/FileIO.java
@@ -3,6 +3,15 @@ package src;
 import javafx.scene.control.Alert;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
+import org.apache.tika.Tika;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.opendocument.OpenOfficeParser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.bouncycastle.asn1.cms.MetaData;
+import org.xml.sax.SAXException;
 
 import java.io.*;
 
@@ -15,30 +24,21 @@ public class FileIO
         // Setting up file chooser
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open a File");
-        fileChooser.getExtensionFilters().add(
-            new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        fileChooser.getExtensionFilters().addAll(
+            new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+            new FileChooser.ExtensionFilter("OpenOffice Documents", "*.odt"));
 
         File selectedFile = fileChooser.showOpenDialog(window);
 
-        // Select correct loader for file type, docx to be implemented later
-        try
+        Tika tika = new Tika();
+
+        try (InputStream is = new FileInputStream(selectedFile))
         {
-            String fileExtension = selectedFile.getName().split("\\.")[1];
-            if (fileExtension.equals("txt"))
-            {
-                return OpenText(selectedFile);
-            } else
-            {
-                Alert alert = new Alert(Alert.AlertType.WARNING);
-                alert.setTitle("Invalid File Extension");
-                alert.setContentText("Please check file and try again.");
-                alert.showAndWait();
-                return null;
-            }
-        } catch (NullPointerException e) // NullPointer will occur if user cancels open
-        {
-            return null;
+            return tika.parseToString(is);
+        } catch (IOException | TikaException e){ // TODO: Proper error handling
+
         }
+        return "";
     }
 
     protected void Save(String text, Window window)  // Needs to change to bool if save-on-change feature implemented
@@ -72,28 +72,5 @@ public class FileIO
             alert.setContentText(e.getMessage());
             alert.showAndWait();
         }
-    }
-
-    private String OpenText(File file)
-    {
-        // String builder and buffered reader to allow for reading large text files
-        StringBuilder text = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(file)))
-        {
-            String curLine = reader.readLine();
-
-            while (curLine != null)
-            {
-                text.append(curLine).append("\n");
-                curLine = reader.readLine();
-            }
-        } catch (IOException e)
-        {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error attempting to open");
-            alert.setContentText(e.getMessage());
-            alert.showAndWait();
-        }
-        return text.toString();
     }
 }
